fd067b6cefa1eea0b3f7cd341c1ccf17ad91bd81
daniel.boros <daniel.boros@ocado.com>
2021-05-01 16:35:35 +0100

    Implement Hat's draw method

--- prob_calculator.py
+++ prob_calculator.py
@@ -9,7 +9,7 @@ class Hat:
     def _set_content(self, balls=None):
 
         if not balls:
-            return ["Red"]
+            return ["red"]
 
         cont = []
         for ballColour, amount in balls.items():
@@ -17,6 +17,19 @@ class Hat:
             cont += colour
 
         return cont
+    
+    def draw(self, amount):
+        # Randomly picks "amount" of balls from content and removes it with pop()
+        if len(self.content) == 0:
+            return []
+        
+        random_index = random.randint(0, len(self.content) - 1)
+        pickedItem = self.content.pop(random_index)
+        if amount == 1:
+            return [pickedItem]
+        
+        else:
+            return [pickedItem] + self.draw(amount - 1)
         
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
