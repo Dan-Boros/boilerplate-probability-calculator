cf242c699a0ffcd8afdd7f2bd6214dafbf991d23
daniel.boros <daniel.boros@ocado.com>
2021-05-01 16:15:34 +0100

    Impelement Hat content init

--- prob_calculator.py
+++ prob_calculator.py
@@ -3,6 +3,21 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
+    def __init__(self, **balls):
+        self.content = self._set_content(balls)
+    
+    def _set_content(self, balls=None):
 
+        if not balls:
+            return ["Red"]
+
+        cont = []
+        for ballColour, amount in balls.items():
+            colour = [ballColour] * amount
+            cont += colour
+
+        return cont
+        
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    pass
